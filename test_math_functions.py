5f17e59d13944d56aa81d09db7180253ed8160f0
Hovhannisyan111 <ogannisyan.2004.01@gmail.com>
2024-11-12 12:56:52 +0400

    testing

---
+++ test_math_functions.py
@@ -0,0 +1,9 @@
+# test_math_functions.py
+
+from math_functions import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-2, -3) == -5
