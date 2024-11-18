10b6ad6d7e30806067b319a25868f4a26b07bb65
Hovhannisyan111 <ogannisyan.2004.01@gmail.com>
2024-11-18 13:32:35 +0400

    NEw

--- py_files/test_math_functions.py
+++ py_files/test_math_functions.py
@@ -1,9 +1,5 @@
-# test_math_functions.py
+# math_functions.py
 
-from math_functions import add
-
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
-    assert add(-2, -3) == -5
+def add(a, b):
+    """Returns the sum of a and b."""
+    return a + b
